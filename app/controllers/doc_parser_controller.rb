8facbc78223d32b6e3c75f560a3ef29d128f0fcd
Tyler DeWitt <tyler.dewitt@colorado.edu>
2018-08-29 22:52:45 +1000

    Do more things

--- app/controllers/doc_parser_controller.rb
+++ app/controllers/doc_parser_controller.rb
@@ -4,6 +4,8 @@ class DocParserController < ApplicationController
 
   def insert
     puts params
-    Meter.where(name: params['meter_name']).first_or_create({name: params['meter_name']})
+    meter = Meter.where(name: params['meter_name']).first_or_create({name: params['meter_name']})
+    meter.update_attribute(:number, params['meter_number'])
+    meter.save!
   end
 end
